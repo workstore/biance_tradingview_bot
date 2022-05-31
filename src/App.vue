c6dd11eb76b8fed4adb0833c1a4361ae6c8e58b6
jason chang <jasonchang6435@gmail.com>
2022-05-31 17:11:30 +0800

    update validate

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,7 @@
 </style>
 
 <style lang="scss">
+@import "element-plus/theme-chalk/src/dark/css-vars.scss";
 @import "element-plus/dist/index.css";
+// @import "./styles/dark/css-vars.css";
 </style>
--- src/views/Entry/index.vue
+++ src/views/Entry/index.vue
@@ -1,66 +1,95 @@
 <template>
   <div class="email-wrapper">
     <div class="header">POD shared this document with you</div>
-    <div class="sub-title">Please enter your info to continue</div>
+    <p class="sub-title">Please enter your info to continue</p>
     <ElForm
-      :ref="formRef"
+      ref="formRef"
+      :rules="rules"
       :model="formLabelAlign"
       label-position="top"
-      label-width="100px"
-      style="max-width: 460px"
+      label-width="120px"
+      class="email-form"
+      size="large"
     >
-      <ElFormItem label="Email">
+      <ElFormItem label="Email" prop="email">
         <ElInput
           v-model="formLabelAlign.email"
           placeholder="Please input your email..."
         />
       </ElFormItem>
-      <div class="sub-title">
-        The information will only be shared with POD 3DShuttle
+      <div class="tip-title">
+        The information will only be shared with
+        <em><strong>POD 3DShuttle</strong></em>
       </div>
       <ElFormItem>
-        <ElButton @click="passEmail()">Continue</ElButton>
+        <button
+          type="button"
+          class="form-button single-button email-button"
+          @click="passEmail(formRef)"
+        >
+          Continue
+        </button>
       </ElFormItem>
     </ElForm>
   </div>
 </template>
 <script lang="ts" setup>
 import { ref, reactive } from "vue";
-// import type { Ref } from "vue";
-import { ElMessage } from "element-plus";
 import type { FormInstance, FormRules } from "element-plus";
 import { useRouter } from "vue-router";
+import { useCssVar } from "@vueuse/core";
+
+const el = ref(null);
+const color = useCssVar("--el-color-primary", el);
+color.value = "#071215";
 
 const router = useRouter();
 
 const formRef = ref<FormInstance>();
-
 const formLabelAlign = reactive({
-  email: ref(""),
+  email: "",
 });
 
-const validateEmail = (email: string) => {
+const validateEmail = (mail: string) => {
   const pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  return pattern.test(email);
+  return pattern.test(mail);
 };
 
-const email = ref<string>("");
-
-const passEmail = (): void => {
-  if (validateEmail(formLabelAlign.email)) {
-    router.push({
-      name: "NFTForm",
-      query: {
-        email: encodeURIComponent(formLabelAlign.email),
+const rules = reactive(<FormRules>{
+  email: [
+    {
+      required: true,
+      trigger: "blur",
+      validator: (rule: any, value: any, callback: any) => {
+        if (!value) {
+          callback(new Error("Please input your email"));
+        }
+        if (!validateEmail(value)) {
+          callback(new Error("Please input a valid email"));
+        }
+        callback();
       },
-    });
-  } else {
-    // TODO
-    ElMessage({
-      message: "Invalid Email!",
-      type: "error",
-    });
+    },
+  ],
+});
+
+const passEmail = async (formEle: any) => {
+  if (!formEle) {
+    return;
   }
+  await formEle.validate((valid: boolean) => {
+    console.log("debug valid", valid);
+    if (valid) {
+      router.push({
+        name: "NFTForm",
+        query: {
+          email: encodeURIComponent(formLabelAlign.email),
+        },
+      });
+    } else {
+      console.log("error submit!");
+    }
+  });
 };
 </script>
 <style lang="scss" scoped>
@@ -78,16 +107,38 @@ const passEmail = (): void => {
   cursor: auto;
 }
 .header {
+  font-family: "WhyteInktrapRegular";
   font-size: 24px;
   font-weight: 500;
   color: #000;
   text-align: center;
+  margin-top: 0.5rem;
 }
 
 .sub-title {
-  font-size: 16px;
+  font-size: 14px;
   color: #696c76;
-  margin-top: 10px;
+  margin-top: 6px;
   text-align: center;
+}
+
+.email-form {
+  width: 90%;
+  margin-top: 35px;
+  margin: 40px auto 0;
+  max-width: 460px;
+}
+
+.tip-title {
+  font-size: 14px;
+  color: #696c76;
+  margin-top: 6px;
+  text-align: left;
+  position: relative;
+  top: -16px;
+}
+
+.email-button {
+  margin: 30px auto 0;
 }
 </style>
