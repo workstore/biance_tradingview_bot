5c8992e2828b7c041b48201df60c05abf993e8f5
Jasonchang6435 <jasonchang@gmail.com>
2023-12-09 21:18:05 +0800

    feat: server dev

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const app = express();
 
 // all environments
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 80); // 4000
 app.set("views", path.join(__dirname, "views"));
 // app.set('view engine', 'pug')
 // app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
